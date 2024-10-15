31e015915d0d7d6729cdd23adcc585d286c00b84
katherinemoriss-pvcc <kconley7321@email.vccs.edu>
2024-10-15 10:16:36 -0400

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,13 @@
-# Name: Katherine Morris
-# Prog Purpose: Hello World
-
-print("Hello World! This is my first Python program.")
-print("I can program in HTML, which is awsome!")
-print("However, HTML cannot do arithmetic.")
-print("But Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide.")
-print("and lots of other things!")
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/Kwich/OneDrive/Desktop/PYTHON/helloworld.py ========
+Hello World! This is my first Python program.
+I can program in HTML, which is awsome!
+However, HTML cannot do arithmetic.
+But Python can: 
+	 Add 
+	 Subtract 
+	 Multiply 
+	 Divide.
+and lots of other things!
