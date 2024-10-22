51549635e2a966be0ccaac6faa64342ef34fa68c
katherinemoriss-pvcc <kconley7321@email.vccs.edu>
2024-10-22 16:54:55 -0400

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,8 @@
-# Name: Katherine Morris
+# Katherine Morris
 # Prog Purpose: Hello World
 
 print("Hello World! This is my first Python program.")
-print("I can program in HTML, which is awsome!")
+print("I can program in HTML, which is awsome.")
 print("However, HTML cannot do arithmetic.")
-print("But Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide.")
+print("But Python can: \nt Add \n\t Subtract \n\t Multiply \n\t Divide.")
 print("and lots of other things!")
