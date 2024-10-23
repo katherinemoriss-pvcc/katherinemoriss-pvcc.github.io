580cce665293f6710c76c2c910a16dd5a3824827
katherinemoriss-pvcc <kconley7321@email.vccs.edu>
2024-10-23 15:39:29 -0400

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -4,5 +4,5 @@
 print("Hello World! This is my first Python program.")
 print("I can program in HTML, which is awsome.")
 print("However, HTML cannot do arithmetic.")
-print("But Python can: \nt Add \n\t Subtract \n\t Multiply \n\t Divide.")
+print("But Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide.")
 print("and lots of other things!")
