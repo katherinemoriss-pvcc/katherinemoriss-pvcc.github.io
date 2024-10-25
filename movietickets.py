17fc8c940d3f767285817c8f2b8dffa952d7f8d2
katherinemoriss-pvcc <kconley7321@email.vccs.edu>
2024-10-25 17:12:35 -0400

    Add files via upload

---
+++ movietickets.py
@@ -0,0 +1,48 @@
+# Katherine Morris
+# Prog Purpose: This program finds the cost of movie tickets.
+
+#   Price for one ticket: $10.99
+#   Sales tax rate: 5.5%
+
+import datetime
+########## difine glabal variables ##########
+# define tax rate and prices
+SALES_TAX_RATE = .055
+PR_TICKET = 10.99
+
+# define global variables
+num_tickets = 0
+subtotal = 0
+salestax = 0.055
+total = 0
+
+########## define program functions ##########
+def main():
+    get_user_data()
+    perform_calculations()
+    display_results
+
+def get_user_data():
+    global num_tickets
+    num_tickets = int(input())
+
+def perform_calculations():
+    global subtotal, sales_tax, total
+    subtotal=num_tickets *PR_TICKET
+    sales_tax = subtotal*SALES_TAX_RATE
+    total = subtotal + sales_tax
+
+def display_results():
+    print('--------------------------')
+    print('****CINEMA HOUSE MOVIES****')
+    print('Your neighborhood movie house')
+    print('----------------------------')
+    print('Tickets    $ ' +str(subtotal, '8,.2f'))
+    print('Sales Tax   $ ' +str(sales_tax, '8,.2f'))
+    print('Total       $ ' +str(total, '8,.2f'))
+    print('------------------------------')
+    print(str(datetime.datetime.now()))
+
+########### call on main program to execute #############
+main()
+
