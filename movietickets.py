33790a998c1be72329f0cac5f93c656b7b6312c9
katherinemoriss-pvcc <kconley7321@email.vccs.edu>
2024-10-26 14:45:33 -0400

    Add files via upload

--- movietickets.py
+++ movietickets.py
@@ -11,26 +11,26 @@ SALES_TAX_RATE = .055
 PR_TICKET = 10.99
 
 # define global variables
-num_tickets = 0
+num_tickets = 2
 subtotal = 0
 salestax = 0.055
 total = 0
 
 ########## define program functions ##########
 def main():
-    get_user_data()
-    perform_calculations()
-    display_results
+    
+    more_tickets: true
+    
+    while more_tickets:
+       get_user_data()
+       perform_calculations()
+       display_results
 
-def get_user_data():
-    global num_tickets
-    num_tickets = int(input())
-
-def perform_calculations():
-    global subtotal, sales_tax, total
-    subtotal=num_tickets *PR_TICKET
-    sales_tax = subtotal*SALES_TAX_RATE
-    total = subtotal + sales_tax
+       yesno==input("Would you like to order again (y or no)?")
+       if yesno=="N" or yesno== "n":
+           more_tickets=False
+           print("Thank you for your order. Enjoy your movie!")
+           total = subtotal + sales_tax
 
 def display_results():
     print('--------------------------')
