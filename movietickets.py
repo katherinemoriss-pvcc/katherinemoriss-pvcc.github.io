0ad4b5ea5ae11e33f1aefa1ed96329996a17539c
katherinemoriss-pvcc <kconley7321@email.vccs.edu>
2024-11-05 19:55:03 -0500

    Add files via upload

--- movietickets.py
+++ movietickets.py
@@ -1,45 +1,58 @@
 # Katherine Morris
 # Prog Purpose: This program finds the cost of movie tickets.
-
 #   Price for one ticket: $10.99
 #   Sales tax rate: 5.5%
 
 import datetime
-########## difine glabal variables ##########
+
+################ difine glabal variables ##################
 # define tax rate and prices
 SALES_TAX_RATE = .055
 PR_TICKET = 10.99
-
-# define global variables
-num_tickets = 2
+     
+# difine global variables
+num_tickets = 0
 subtotal = 0
-salestax = 0.055
+sales_tax = 0
 total = 0
 
 ########## define program functions ##########
 def main():
-    
-    more_tickets: true
-    
+
+    more_tickets = True
+      
+
     while more_tickets:
-       get_user_data()
-       perform_calculations()
-       display_results
+        get_user_data()
+        perform_calculations()
+        display_results()
 
-       yesno==input("Would you like to order again (y or no)?")
-       if yesno=="N" or yesno== "n":
-           more_tickets=False
-           print("Thank you for your order. Enjoy your movie!")
-           total = subtotal + sales_tax
+        yesno = input("\nWould you like to order again (Y or N)?")
+        if yesno == "N" or yesno =="n":
+           more_tickets = False
+           print("Thank you for your order. Come again!")  
+            
 
+def get_user_data():
+    global num_tickets
+    num_tickets = int(input("Number of movie tickets:"))
+
+    
+def perform_calculations():
+     global subtotal, sales_tax, total
+     subtotal = num_tickets * PR_TICKET
+     sales_tax = subtotal * SALES_TAX_RATE
+     total = subtotal + sales_tax
+
+         
 def display_results():
     print('--------------------------')
     print('****CINEMA HOUSE MOVIES****')
     print('Your neighborhood movie house')
     print('----------------------------')
-    print('Tickets    $ ' +str(subtotal, '8,.2f'))
-    print('Sales Tax   $ ' +str(sales_tax, '8,.2f'))
-    print('Total       $ ' +str(total, '8,.2f'))
+    print('Tickets     $ ' +format(subtotal,'8,.2f'))
+    print('Sales Tax   $ ' +format(sales_tax,'8,.2f'))
+    print('Total       $ ' +format(total,'8,.2f'))
     print('------------------------------')
     print(str(datetime.datetime.now()))
 
